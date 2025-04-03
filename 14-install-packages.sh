2cfca7c091e8a1935878ad276f8708f72f453638
soumya <soumyamunni510@gmail.com>
2025-04-03 21:20:01 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -11,10 +11,10 @@ Y="\e[33m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2...FAILURE"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo -e "$2...$G SUCCESS $N"
     fi 
 }
 if [ $USERID -ne 0 ]
@@ -33,7 +33,8 @@ do
     then
         echo -e "$i already installed...$Y SKIPPING $N"
     else
-        echo "$i not installed...Need to install"
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
     fi
 done
 
